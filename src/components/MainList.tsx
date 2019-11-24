6011d66a289d96bd270a2d5f335aec44f2904c7a
Kong Chae Won <chaewonkong@gmail.com>
2019-11-24 16:49:38 +0900

    feat: add all types and apis

--- src/components/MainList.tsx
+++ src/components/MainList.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { View, Text, ScrollView } from 'react-native'
 import { API } from "../api"
-import { ISearchMakkchaParam, ISubway } from "../shared-interfaces"
+import { ISearchMakkchaParam, IBusAndSubway, ITransportations, ITaxi, IDdarng, IOwlBus } from "../shared-interfaces"
 
 interface IProps {
 
@@ -10,16 +10,25 @@ interface IProps {
 
 const MainList: React.FC<IProps> = props => {
     const pointsConfig = {
-        startX: 126.7508563,
-        startY: 37.487626,
-        endX: 126.7208015,
-        endY: 37.4905169,
+        startX: 127.0485058,
+        startY: 37.5827188,
+        endX: 127.0083575,
+        endY: 37.5613504,
     }
     const getData = async (pointsConfig: ISearchMakkchaParam): Promise<void> => {
-        const data = await API.getSubway(pointsConfig)
-        setData(data)
+        const dataFetchPromises: [Promise<IBusAndSubway[]>, Promise<IBusAndSubway[]>, Promise<IBusAndSubway[]>, Promise<ITaxi>, Promise<IOwlBus[]>, Promise<IDdarng[]>] = [
+            API.getSubway(pointsConfig),
+            API.getBus(pointsConfig),
+            API.getBusAndSubway(pointsConfig),
+            API.getTaxi(pointsConfig),
+            API.getOwlBus(pointsConfig),
+            API.getDdarng(pointsConfig)
+        ];
+
+        const [subway, bus, busAndSubway, taxi, owlbus, ddarng] = await Promise.all(dataFetchPromises)
+        setData({ subway, bus, busAndSubway, taxi, owlbus, ddarng })
     }
-    const [data, setData] = React.useState<ISubway[]>();
+    const [data, setData] = React.useState<ITransportations>();
     React.useEffect(() => {
         getData(pointsConfig)
     }, []);
--- src/shared-interfaces.ts
+++ src/shared-interfaces.ts
@@ -8,8 +8,17 @@ export interface ISearchMakkchaParam {
   curfewMin?: number | null
 }
 
+export interface ITransportations {
+  bus: IBusAndSubway[],
+  subway: IBusAndSubway[],
+  busAndSubway: IBusAndSubway[],
+  owlbus: IOwlBus[],
+  taxi: ITaxi,
+  ddarng: IDdarng[]
+}
+
 // Interface for search subway res
-export interface ISubway {
+export interface IBusAndSubway {
   type: string;
   longName: string | null;
   transferNum: number;
@@ -17,11 +26,52 @@ export interface ISubway {
   lastTime: string;
   remain: number;
   operationType: string;
-  stepList: ISubwayStep[]
+  stepList: IStep[]
 }
 
+export interface ITaxi {
+  time: number;
+  price: number;
+  distance: number;
+}
+
+export interface IDdarng {
+  pointX: number;
+  pointY: number;
+  id: number;
+  location: string;
+  stationId: string;
+  totalCnt: number;
+  curCnt: number;
+  walkDuration: number;
+}
+
+export interface IOwlBus {
+  type: "OWL_BUS";
+  totalTime: number;
+  timetable: string;
+  stepList: IStep[];
+  remain: number;
+  price: number;
+}
+
+/*
+        "type": "WALKING",
+                "from": null,
+                "to": null,
+                "fromPoint": null,
+                "toPoint": null,
+                "duration": 0,
+                "iconColor": "#707070",
+                "iconName": null,
+                "name": null,
+                "operationType": null,
+                "price": 0,
+                "nextStation": null,
+*/
+
 // Interface for each steps
-export interface ISubwayStep {
+export interface IStep {
   type: string,
   from: string | null;
   to: string | null;
@@ -30,14 +80,14 @@ export interface ISubwayStep {
   duration: number;
   iconColor: string;
   iconName: string | null;
-  headsign: string | null;
-  door: string | null;
+  headsign?: string | null;
+  door?: string | null;
   name: string | null;
   operationType: string | null;
-  departureTime: string | null;
+  departureTime?: string | null;
   nextStation: string | null;
   points: IXYPoints[]
-  options: string | null;
+  price?: number;
 }
 
 // Interface for x,y coords
